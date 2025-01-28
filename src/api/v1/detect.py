373c0e008143daee0fb879991a7e65fcb85bbab7
kuwacom <kuwa.com3@gmail.com>
2025-01-29 03:41:21 +0900

    update: 複数画像に対応

--- src/api/v1/detect.py
+++ src/api/v1/detect.py
@@ -1,19 +1,16 @@
 from fastapi import APIRouter, File, Form, UploadFile, HTTPException
-from fastapi.responses import StreamingResponse
 from typing import List
 
 import cv2
-import numpy as np
-from io import BytesIO
-from models.detect import DetectRequest
-
-from utils import modelManager, configManager
-from PIL import Image
+import torch
 import json
+import numpy as np
+from models.detect import DetectRequest, DetectResponse
+from utils import modelManager, configManager, converter
 
 detectRouter = APIRouter()
 
-@detectRouter.post("/detect")
+@detectRouter.post("/detect", response_model=List[DetectResponse])
 async def detect(
     config: str = Form(...), 
     images: List[UploadFile] = File(...)
@@ -35,17 +32,19 @@ async def detect(
             imageDatas.append(cv2.imdecode(imageArray, cv2.IMREAD_COLOR))
 
         # imageData = Image.open(BytesIO(imageBinary))
-            
+
         model = modelManager.getModel(configObj.model)
         results = model(
             imageDatas,
             conf=configObj.confidenceThreshold or configManager.config.get('detection', 'defaultConfidenceThreshold'),
             iou=configObj.nmsThreshold or configManager.config.get('detection', 'defaultNmsThreshold'),
-            imgsz=640,
+            imgsz=640, # 処理する際の画像解像度
             half=configManager.config.getboolean('detection', 'half'),
             device=configManager.config.get('detection', 'device')
         )
 
+        # 結果は公式documentを参照
+        # https://docs.ultralytics.com/modes/predict/#working-with-results
         # for result in results:
         #     boxes = result.boxes  # Boxes object for bounding box outputs
         #     masks = result.masks  # Masks object for segmentation masks outputs
@@ -55,18 +54,35 @@ async def detect(
         #     result.show()  # display to screen
         #     result.save(filename="result.jpg")  # save to disk
 
-        # Assuming results[0] contains the image in numpy format
-        imgArray = results[0].plot()  # Get the numpy array of the image
-        # print(results)
+        detectResponse: List[DetectResponse] = []
         for result in results:
-            print(result.boxes)
-        # imgPil = Image.fromarray(imgArray)  # Convert numpy array to PIL Image
+            # 検出されたタグクラス名の量をオブジェクトで表現 例: {0: 2, 12: 2} 
+            names, counts = torch.unique(result.boxes.cls, return_counts=True)
+            namesCount = dict(zip(
+                names.to(torch.int).tolist(),
+                counts.tolist()
+            ))
 
-        _, imgEncoded = cv2.imencode('.jpg', imgArray)
-        # エンコードした画像をメモリ内に保存
-        imgIO = BytesIO(imgEncoded.tobytes())
+            print(namesCount)
 
-        return StreamingResponse(imgIO, media_type="image/jpeg")
+            base64Image = ""
+            if configObj.detectedImage:
+                base64Image = converter.ndarray2base64(result.plot())
+            
+            detectResponse.append(DetectResponse(
+                model=configObj.model,
+                confidenceThreshold=configObj.confidenceThreshold,
+                nmsThreshold=configObj.nmsThreshold,
+                namesCount=namesCount,
+                detectedImage=base64Image,
+                detectTime=result.speed,
+                xywh=result.boxes.xywh.tolist(),
+                xywhn=result.boxes.xywhn.tolist(),
+                xyxy=result.boxes.xyxy.tolist(),
+                xyxyn=result.boxes.xyxyn.tolist(),
+            ))
+
+        return detectResponse
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- src/models/detect.py
+++ src/models/detect.py
@@ -1,14 +1,22 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Dict, List
 
 class DetectRequest(BaseModel):
     model: str
     confidenceThreshold: Optional[float] = 0.5
     nmsThreshold: Optional[float] = 0.4
-    plotImage: bool = False
+    detectedImage: bool = False
 
 class DetectResponse(BaseModel):
     model: str
     confidenceThreshold: Optional[float]
     nmsThreshold: Optional[float]
+    namesCount: Dict[int, int]
+    detectedImage: str = ""
+    detectTime: Dict
+    xywh: List[List[float]]
+    xywhn: List[List[float]]
+    xyxy: List[List[float]]
+    xyxyn: List[List[float]]
+
 

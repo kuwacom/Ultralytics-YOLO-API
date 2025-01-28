e1f995b88a8a0f271332c998f75d4b3de60f0969
kuwacom <kuwa.com3@gmail.com>
2025-01-29 02:41:39 +0900

    update: 型を追加

--- src/utils/modelManager.py
+++ src/utils/modelManager.py
@@ -1,16 +1,17 @@
 from ultralytics import YOLO
+from typing import Dict, Optional
 
-Models = {}
+Models: Dict[str, YOLO] = {}
 
-def getModel(modelName):
+def getModel(modelName: str):
     if modelName in Models:
         return Models[modelName]
     else:
         return None
 
-def addModel(modelName, model):
+def addModel(modelName: str, model: YOLO):
     Models[modelName] = model
 
-def initModels(models):
+def initModels(models: list[tuple[str, str]]):
     for modelName, modelPath in models:
         addModel(modelName, YOLO(modelPath))

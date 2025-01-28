75d9644cc997553c67c5d2c989aa2be6699e82d1
kuwacom <kuwa.com3@gmail.com>
2025-01-29 03:16:06 +0900

    update: model 一覧取得エンドポイントのレスポンスを変更
    判別クラス名リストを追加

--- src/api/v1/models.py
+++ src/api/v1/models.py
@@ -1,13 +1,19 @@
 from typing import List
 from fastapi import APIRouter, HTTPException
-import utils
-import json
+from utils import modelManager
+from models.models import Model
 
 modelsRouter = APIRouter()
 
-@modelsRouter.get("/models", response_model=List[str])
+@modelsRouter.get("/models", response_model=List[Model])
 async def detect():
     try:
-        return list(utils.modelManager.Models.keys())
+        responseList = []
+        for modelName in modelManager.Models.keys():
+            responseList.append(Model(
+                name=modelName,
+                names=modelManager.getModel(modelName).names
+            ))
+        return responseList
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))

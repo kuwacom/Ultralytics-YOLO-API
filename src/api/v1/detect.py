9994ed3935e7f06d8b7e94eeb7bdfe6d574ad7be
kuwacom <kuwa.com3@gmail.com>
2025-01-29 02:15:05 +0900

    wip: config取得の修正と複数ファイルの認識に対応

--- src/api/v1/detect.py
+++ src/api/v1/detect.py
@@ -1,32 +1,46 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi import APIRouter, File, Form, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
+from typing import List
+
+import cv2
+import numpy as np
 from io import BytesIO
 from models.yolo11.detect import DetectRequest
 
 from utils import modelManager, configManager
 from PIL import Image
+import json
 
 detectRouter = APIRouter()
 
 @detectRouter.post("/detect")
 async def detect(
-    config: DetectRequest, 
-    image: UploadFile = File(...)
+    config: str = Form(...), 
+    images: List[UploadFile] = File(...)
 ):
     try:
+        configData = json.loads(config)
+        configObj = DetectRequest(**configData)
+
         # 存在しないモデル名が指定された時
-        if not config.model:
-            config.model = configManager.config.get('detection', 'defaultModel')
-        elif not (config.model in configManager.config):
+        if not configObj.model:
+            configObj.model = configManager.config.get('detection', 'defaultModel')
+        elif not (configObj.model in configManager.config['models']):
             raise HTTPException(status_code=400, detail="model not found in config")
-        
-        imageData = await image.read()
-        
-        model = modelManager.getModel(config.model)
-        results = model.predict(
-            imageData,
-            conf=config.confidenceThreshold or configManager.config.get('detection', 'defaultConfidenceThreshold'),
-            iou=config.nmsThreshold or configManager.config.get('detection', 'defaultNmsThreshold'),
+
+        imageDatas = []
+        for image in images:
+            imageBinary = await image.read()
+            imageArray = np.frombuffer(imageBinary, np.uint8)
+            imageDatas.append(cv2.imdecode(imageArray, cv2.IMREAD_COLOR))
+
+        # imageData = Image.open(BytesIO(imageBinary))
+            
+        model = modelManager.getModel(configObj.model)
+        results = model(
+            imageDatas,
+            conf=configObj.confidenceThreshold or configManager.config.get('detection', 'defaultConfidenceThreshold'),
+            iou=configObj.nmsThreshold or configManager.config.get('detection', 'defaultNmsThreshold'),
             imgsz=640,
             half=configManager.config.getboolean('detection', 'half'),
             device=configManager.config.get('detection', 'device')
@@ -42,17 +56,15 @@ async def detect(
         #     result.save(filename="result.jpg")  # save to disk
 
         # Assuming results[0] contains the image in numpy format
-        imgArray = results[0].numpy  # Get the numpy array of the image
-        imgPil = Image.fromarray(imgArray)  # Convert numpy array to PIL Image
+        imgArray = results[0].plot()  # Get the numpy array of the image
+        # print(results)
+        for result in results:
+            print(result.boxes)
+        # imgPil = Image.fromarray(imgArray)  # Convert numpy array to PIL Image
 
-        imgIO = BytesIO()
-        imgPil.save(imgIO, format='JPEG')  # Save the PIL Image to BytesIO
-        imgIO.seek(0)
-
-        # 結果の画像をバイナリとして返す
-        # imgIO = BytesIO()
-        # results[0].save(imgIO, 'JPEG')
-        # imgIO.seek(0)
+        _, imgEncoded = cv2.imencode('.jpg', imgArray)
+        # エンコードした画像をメモリ内に保存
+        imgIO = BytesIO(imgEncoded.tobytes())
 
         return StreamingResponse(imgIO, media_type="image/jpeg")
     
